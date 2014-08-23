025b2685b58e51d7a1fb8e0b83ae515495ddbf43
jaanumahe <write2mahen@gmail.com>
2014-08-23 14:05:17 -0700

    JS File Added.

---
+++ LearnJavascript.js
@@ -0,0 +1,18 @@
+/**
+ * @author Mahendran Pandurangan
+ */
+
+/*var webdriver = require('selenium-webdriver');
+ 
+var driver = new webdriver.Builder().
+   withCapabilities(webdriver.Capabilities.chrome()).
+   build();
+ 
+driver.get('http://www.google.com');
+driver.findElement(webdriver.By.name('q')).sendKeys('simple programmer');
+driver.findElement(webdriver.By.name('btnG')).click();*/
+
+var a=1,b=0;
+sum=a+b;
+print sum;
+
